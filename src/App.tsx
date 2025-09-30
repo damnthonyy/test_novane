888c9be68a78f469b403e971932db2c9b43e1932
damnthonyy <mahassadijoseph@gmail.com>
2025-09-30 13:04:12 +0200

    init 👨🏾‍🎨

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+import './assets/styles/Global.css'
+
+function App() {
+
+  return (
+    <div className='flex justify-center h-[100vh] items-center'>
+      <h1 className='font-bold uppercase '>Hello world</h1>
+    </div>
+  )
+}
+
+export default App
